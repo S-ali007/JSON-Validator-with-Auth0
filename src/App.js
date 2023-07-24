feffdad82fc2bf093294bf74b5e7cb64d4b5a71c
ALi <Ha3er125@teachers.org>
2023-07-24 22:39:29 +0500

    error handling and setting web origins on Auth0  also set  routing

--- src/App.js
+++ src/App.js
@@ -27,8 +27,8 @@ function App() {
     <>
       <Routes>
         <Route path={"/signup"} element={<SignupPage />} />
-        <Route path={"/"} element={<LoginPopup setToken={setToken} />} />
-       {token ? <Route path={"/home"} element={<All_Components setdata={setdata} />} />:"" }
+        
+       {token ? <Route path={"/home"} element={<All_Components setdata={setdata} />} />: <Route path={"/"} element={<LoginPopup setToken={setToken} />} />}
         <Route path="/*" element={<Page_404 />} />
       </Routes>
     </>
