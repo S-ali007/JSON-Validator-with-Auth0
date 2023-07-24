a8c93e1f5b20a07f959c0bac1dc59440f9960212
ALi <Ha3er125@teachers.org>
2023-07-25 00:11:22 +0500

    error handling and setting web origins on Auth0  also set  routing

--- src/App.js
+++ src/App.js
@@ -4,31 +4,25 @@ import All_Components from "./Components/All_Components";
 import SignupPage from "./Features/Signup";
 import LoginPopup from "./Features/LoginPopup";
 import Page_404 from "./Components/Page_404";
-import { Link, useNavigate } from "react-router-dom";
-
 
 function App() {
   const [token, setToken] = useState(null);
   const [data, setdata] = useState("");
-  const navigate = useNavigate();
-
 
   const storedToken = sessionStorage.getItem("username");
-  console.log(storedToken)
-  // useEffect(() => {
-  //   if (storedToken) {
-  //     setToken(storedToken);
-  //     // navigate("/")
-       
-  //   }
-  // }, [storedToken]);
+
+  useEffect(() => {
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, [storedToken]);
 
   return (    
     <>
       <Routes>
         <Route path={"/signup"} element={<SignupPage />} />
-        
-         <Route path={"/home"}  element={storedToken ?<All_Components setdata={setdata} />: <LoginPopup setToken={setToken}/> }/> 
+       <Route path={"/"} element={<LoginPopup setToken={setToken} />} />
+      <Route path={"/home"} element={<All_Components setdata={setdata} />} />
         <Route path="/*" element={<Page_404 />} />
       </Routes>
     </>
