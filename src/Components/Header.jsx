a977e518781a379e548529302ab6ef1c0fd20c8b
ALi <Ha3er125@teachers.org>
2023-07-24 22:31:53 +0500

    error handling and setting web origins on Auth0  also set  routing

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -12,7 +12,7 @@ function Header({ setToken, extraclasses }) {
     const webAuth = new auth0.WebAuth({
       domain: "techtribe.us.auth0.com",
       clientID: "ffbSF4A20lHnWOs1A6TuXpVZ0jESDGgY",
-      redirectUri: "http://localhost:3000/home",
+      redirectUri: "https://localhost:3000/home",
     });
 
     const parseAccessToken = () => {
