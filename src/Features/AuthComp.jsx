e8b39eb056a73f35e97fd0b9127f5c32c85ce446
ALi <Ha3er125@teachers.org>
2023-07-27 22:20:56 +0500

    commented navigate

--- src/Features/AuthComp.jsx
+++ src/Features/AuthComp.jsx
@@ -4,8 +4,10 @@ import { Navigate } from 'react-router-dom'
 function AuthComp({children}) {
 
     const uData =JSON.parse(sessionStorage.getItem("username"))
+    console.log(uData)
         if(uData){
             return children
+            
         }
         else{
            return <Navigate to='/'/>
