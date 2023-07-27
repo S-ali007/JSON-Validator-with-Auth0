ff8c7d2b05d02a4c2a9e4763e03053cf4583ace4
ALi <Ha3er125@teachers.org>
2023-07-27 22:40:21 +0500

    commented navigate

--- src/Features/AuthComp.jsx
+++ src/Features/AuthComp.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Navigate } from 'react-router-dom'
 
 function AuthComp({children}) {
-
+    useEffect(()=>{
+    console.log(sessionStorage.getItem("username"),`sessionStorage.getItem("username")`)
+    },[])
     const uData =JSON.parse(sessionStorage.getItem("username"))
+  
     console.log(uData)
         if(uData){
             return children
