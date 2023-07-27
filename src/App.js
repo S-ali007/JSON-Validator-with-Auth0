ba6b384a0e34db64d9ba602a4a1d096435aee65f
ALi <Ha3er125@teachers.org>
2023-07-27 14:59:48 +0500

    commented navigate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { Navigate, Route, Routes, json, useLocation } from "react-router-dom";
 import All_Components from "./Components/All_Components";
 import SignupPage from "./Features/Signup";
 import LoginPopup from "./Features/LoginPopup";
 import Page_404 from "./Components/Page_404";
 import { useAuth0 } from "@auth0/auth0-react";
 import auth0 from "auth0-js";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+
 
 function App() {
-  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const { isAuthenticated } = useAuth0();
   const [userProfile, setUserProfile] = useState(null);
-  const [token, setToken] = useState(null);
-  const [data, setdata] = useState("");
+  const navigate = useNavigate(); 
 
   useEffect(() => {
     const webAuth = new auth0.WebAuth({
       domain: "techtribe.us.auth0.com",
       clientID: "ffbSF4A20lHnWOs1A6TuXpVZ0jESDGgY",
-      redirectUri: "https://https://melodic-cassata-2af0ea.netlify.app/home",
+      redirectUri: "https://melodic-cassata-2af0ea.netlify.app/home",
     });
+
     const parseAccessToken = () => {
       const hash = window.location.hash;
       const tokenIndex = hash.indexOf("access_token=");
@@ -35,7 +36,7 @@ function App() {
 
     const accessToken = parseAccessToken();
     if (accessToken) {
-      webAuth.client.userInfo(accessToken, function (err, user) {
+      webAuth.client.userInfo(accessToken, (err, user) => {
         if (err) {
           console.error("Error fetching user profile:", err);
           return;
@@ -46,21 +47,20 @@ function App() {
 
         sessionStorage.setItem("username", JSON.stringify(user));
         console.log(user, "ali");
-        
       });
 
       if (!JSON.parse(sessionStorage.getItem("username"))) {
-        Navigate("/");
+        navigate("/");
       }
     }
-     }, []);
+  }, []);
 
   return (
     <>
       <Routes>
         <Route path={"/signup"} element={<SignupPage />} />
-        <Route path={"/"} element={<LoginPopup setToken={setToken} />} />
-        <Route path={"/home"} element={<All_Components setdata={setdata} />} />
+        <Route path={"/"} element={<LoginPopup />} />
+        <Route path={"/home"} element={<All_Components />} />
         <Route path="/*" element={<Page_404 />} />
       </Routes>
     </>
