210af62c628a9a4c5783a0fcbf103197b2afa140
ALi <Ha3er125@teachers.org>
2023-07-28 21:21:57 +0500

    fixes in App.js

--- src/App.js
+++ src/App.js
@@ -1,85 +1,173 @@
-import React, { useState, useEffect } from "react";
-import {
-  useNavigate,
-  Route,
-  Routes,
-  json,
-  useLocation,
-} from "react-router-dom";
+// import React, { useState, useEffect } from "react";
+// import {
+//   useNavigate,
+//   Route,
+//   Routes,
+//   json,
+//   useLocation,
+// } from "react-router-dom";
+// import All_Components from "./Components/All_Components";
+// import SignupPage from "./Features/Signup";
+// import LoginPopup from "./Features/LoginPopup";
+// import Page_404 from "./Components/Page_404";
+// import { useAuth0 } from "@auth0/auth0-react";
+// import auth0 from "auth0-js";
+// import AuthComp from "./Features/AuthComp";
+
+// function App() {
+//   const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+//   const [userProfile, setUserProfile] = useState(null);
+//   const [token, setToken] = useState(null);
+//   const [data, setdata] = useState("");
+//   const navigate = useNavigate();
+//   const uData = JSON.parse(sessionStorage.getItem("username"));
+
+//   const [loader, setloader] = useState(false);
+
+//   useEffect(() => {
+//     const webAuth = new auth0.WebAuth({
+//       domain: "techtribe.us.auth0.com",
+//       clientID: "ffbSF4A20lHnWOs1A6TuXpVZ0jESDGgY",
+//       redirectUri: "https://https://melodic-cassata-2af0ea.netlify.app/home",
+//     });
+//     const parseAccessToken = () => {
+//       const hash = window.location.hash;
+//       const tokenIndex = hash.indexOf("access_token=");
+//       if (tokenIndex !== -1) {
+//         const endTokenIndex = hash.indexOf("&", tokenIndex);
+//         const accessToken = hash.substring(
+//           tokenIndex + "access_token=".length,
+//           endTokenIndex !== -1 ? endTokenIndex : undefined
+//         );
+//         return accessToken;
+//       }
+//       return null;
+//     };
+
+//     const accessToken = parseAccessToken();
+//     if (accessToken) {
+//       webAuth.client.userInfo(accessToken, function (err, user) {
+//         if (err) {
+//           console.error("Error fetching user profile:", err);
+//           return;
+//         }
+
+//         // Store the user profile in state
+//         setUserProfile(user);
+//         sessionStorage.setItem("username", JSON.stringify(user));
+//         console.log(user, "ali");
+//       });
+
+//       if (uData) {
+//         // navigate("/home");
+//         console.log("xxxxxxgxx");
+//       }
+//     }
+//   }, []);
+
+//   console.log(loader);
+//   return (
+//     <>
+//       <Routes>
+//         <Route
+//           path={"/signup"}
+//           element={<SignupPage setloader={setloader} />}
+//         />
+//         <Route path={"/"} element={<LoginPopup setToken={setUserProfile} />} />
+//         <Route
+//           path={"/home"}
+//           element={<All_Components setdata={setUserProfile} />}
+//         />
+//         {/* <Route path={"/*"} element={<Page_404 loader={loader} />} /> */}
+//       </Routes>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+// App.js--------------------------------------------------2
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { Auth0Provider } from "@auth0/auth0-react";
+// import All_Components from "./Components/All_Components";
+// import SignupPage from "./Features/SignupPage";
+// import LoginPopup from "./Features/LoginPopup";
+// import Page_404 from "./Components/Page_404";
+
+// const domain = "techtribe.us.auth0.com";
+// const clientId = "ffbSF4A20lHnWOs1A6TuXpVZ0jESDGgY";
+
+// function App() {
+//   return (
+//     <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<LoginPopup />} />
+//           <Route path="/signup" element={<SignupPage />} />
+//           <Route path="/home" element={<All_Components />} />
+//           <Route path="/*" element={<Page_404 />} />
+//         </Routes>
+//       </Router>
+//     </Auth0Provider>
+//   );
+// }
+
+// export default App;
+
+
+
+
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
 import All_Components from "./Components/All_Components";
-import SignupPage from "./Features/Signup";
+import SignupPage from "./Features/SignupPage";
 import LoginPopup from "./Features/LoginPopup";
 import Page_404 from "./Components/Page_404";
-import { useAuth0 } from "@auth0/auth0-react";
-import auth0 from "auth0-js";
-import AuthComp from "./Features/AuthComp";
+
+const domain = "techtribe.us.auth0.com";
+const clientId = "ffbSF4A20lHnWOs1A6TuXpVZ0jESDGgY";
+
+// PrivateRoute component for protected routes
+function PrivateRoute({ children, ...rest }) {
+  const { isAuthenticated } = useAuth0();
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        isAuthenticated ? (
+          children
+        ) : (
+          <LoginPopup redirectTo={location.pathname} />
+        )
+      }
+    />
+  );
+}
 
 function App() {
-  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
-  const [userProfile, setUserProfile] = useState(null);
-  const [token, setToken] = useState(null);
-  const [data, setdata] = useState("");
-  const navigate = useNavigate();
-  const uData =JSON.parse(sessionStorage.getItem("username"))
-
-  const [loader, setloader] = useState(false);
-
-
-  useEffect(() => {
-    const webAuth = new auth0.WebAuth({
-      domain: "techtribe.us.auth0.com",
-      clientID: "ffbSF4A20lHnWOs1A6TuXpVZ0jESDGgY",
-      redirectUri: "https://https://melodic-cassata-2af0ea.netlify.app/home",
-    });
-    const parseAccessToken = () => {
-      const hash = window.location.hash;
-      const tokenIndex = hash.indexOf("access_token=");
-      if (tokenIndex !== -1) {
-        const endTokenIndex = hash.indexOf("&", tokenIndex);
-        const accessToken = hash.substring(
-          tokenIndex + "access_token=".length,
-          endTokenIndex !== -1 ? endTokenIndex : undefined
-        );
-        return accessToken;
-      }
-      return null;
-    };
-
-    const accessToken = parseAccessToken();
-    if (accessToken) {
-      webAuth.client.userInfo(accessToken, function (err, user) {
-        if (err) {
-          console.error("Error fetching user profile:", err);
-          return;
-        }
-
-        // Store the user profile in state
-        setUserProfile(user);
-        sessionStorage.setItem("username", JSON.stringify(user));
-        console.log(user, "ali");
-      });
-
-      if (uData) {
-        // navigate("/home");
-        console.log("xxxxxxgxx")
-      }
-    }
-  }, []);
-
-  console.log(loader)
   return (
-    <>
-      <Routes>
-        <Route path={"/signup"} element={<SignupPage setloader={setloader} />} />
-        <Route path={"/"} element={<LoginPopup setToken={setUserProfile} />} />
-       <Route
-          path={"/home"}
-          element={<All_Components setdata={setUserProfile} />}
-        />
-        {/* <Route path={"/*"} element={<Page_404 loader={loader} />} /> */}
-      </Routes>
-    </>
+    <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPopup />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <PrivateRoute path="/home" element={<All_Components />} />
+          <Route path="/*" element={<Page_404 />} />
+        </Routes>
+      </Router>
+    </Auth0Provider>
   );
 }
 
 export default App;
+
+
--- src/Features/Signup.jsx
+++ src/Features/Signup.jsx
@@ -1,17 +1,164 @@
+// import React, { useState } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import auth0 from "auth0-js";
+
+// function SignupPage({setloader}) {
+//   const [formData, setFormData] = useState({
+//     full_name: "",
+//     email: "",
+//     password: "",
+//   });
+//   const [userProfile, setUserProfile] = useState(null);
+
+//   const navigate = useNavigate();
+  
+
+//   function handleChange(event) {
+//     const { name, value } = event.target;
+//     setFormData((prevFormData) => ({
+//       ...prevFormData,
+//       [name]: value,
+//     }));
+//   }
+
+//   const webAuth = new auth0.WebAuth({
+//     domain: "techtribe.us.auth0.com",
+//     clientID: "ffbSF4A20lHnWOs1A6TuXpVZ0jESDGgY",
+//     redirectUri:"https://melodic-cassata-2af0ea.netlify.app/home"
+//   });
+//   const handleSignup = (e) => {
+//     e.preventDefault();
+//     setloader(true)
+//     const { email, password, full_name } = formData;
+//     webAuth.signup(
+//       {
+//         connection: "JSON-Validator",
+//         email: email,
+//         password: password,
+//         name: full_name,
+//       },
+//       function (err, res) {
+//         if (err) {
+//           console.error("Error signing up:", err);
+//           alert("Error signing up. Please try again later.");
+//           return;
+//         }
+
+//         console.log("Signup successful!");
+
+//         handleLogin(email, password);
+//       }
+//     );
+//   };
+
+//   const handleLogin = (email, password) => {
+
+//     webAuth.login(
+//       {
+//         realm: "JSON-Validator",
+//         username: email,
+//         password: password,
+//         responseType: "token id_token",
+//       },
+//       function (err, authResult) {
+//         if (err) {
+//           console.error("Error logging in:", err);
+//           alert("Error logging in. Please check your credentials.");
+//           return;
+//         }
+//         // navigate("/home")
+//         }
+//     );
+//   };
+
+
+//   return (
+//     <>
+//       <div className="flex justify-center items-center w-full h-[600px]">
+//         <div className="border- border-[2px] max-w-[500px] w-full  rounded-[20px]">
+//           <form
+//             onSubmit={handleSignup}
+//             className="max-w-[500px] w-full p-[2px]"
+//           >
+//             <div className="bg-sky-500  rounded-[20px] text-white">
+//               <h3 className="max-w-[250px] w-full items-center text-center font-bold text-[30px] py-4 ">
+//                 Sign Up
+//               </h3>
+//             </div>
+//             <div className="px-[4px] mt-[20px]">
+//               <div className="mb-3 flex gap-[60px] h-[50px]">
+//                 <label className="w-full max-w-[70px] flex justify-center items-center">
+//                   Full name
+//                 </label>
+//                 <input
+//                   className="form-control w-full border border-blue-300 rounded-[5px] px-[4px] outline-none  hover:border-sky-700 hover:border-b-4"
+//                   placeholder="Full name"
+//                   name="full_name"
+//                   onChange={handleChange}
+//                 />
+//               </div>
+
+//               <div className="mb-3 flex gap-[30px] h-[50px]">
+//                 <label className="w-full max-w-[100px] text-center flex justify-center items-center">
+//                   Email address
+//                 </label>
+//                 <input
+//                   name="email"
+//                   className="form-control w-full border border-blue-300 rounded-[5px] px-[4px] outline-none hover:border-sky-700 hover:border-b-4"
+//                   placeholder="Enter email"
+//                   onChange={handleChange}
+//                 />
+//               </div>
+//               <div className="mb-3 flex gap-[60px] h-[50px]">
+//                 <label className="w-full max-w-[70px] flex justify-center items-center">
+//                   Password
+//                 </label>
+//                 <input
+//                   name="password"
+//                   type="password"
+//                   className="form-control w-full border border-blue-300 rounded-[5px] px-[4px] outline-none hover:border-sky-700  hover:border-b-4"
+//                   placeholder="Enter password"
+//                   onChange={handleChange}
+//                 />
+//               </div>
+//               <div className="d-grid flex justify-center items-center">
+//                 <button
+//                   type="submit"
+//                   className="btn btn-primary border-[2px] font-bold text-sky-500 hover:text-white text-[16px]  rounded-[10px] max-w-[200px] w-full text-center  py-[5px]  hover:bg-sky-500"
+//                 >
+//                   Sign Up
+//                 </button>
+//               </div>
+//             </div>
+//           </form>
+//           <div className="flex items-center justify-center">
+//             <Link to="/">
+//               <button className="mt-4">Already registered? Log in</button>
+//             </Link>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default SignupPage;
+
+
+
+
+// 2---------------------------------
+// SignupPage.js
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import auth0 from "auth0-js";
 
-function SignupPage({setloader}) {
+function SignupPage({ setloader }) {
   const [formData, setFormData] = useState({
     full_name: "",
     email: "",
     password: "",
   });
-  const [userProfile, setUserProfile] = useState(null);
-
-  const navigate = useNavigate();
-  
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -24,11 +171,12 @@ function SignupPage({setloader}) {
   const webAuth = new auth0.WebAuth({
     domain: "techtribe.us.auth0.com",
     clientID: "ffbSF4A20lHnWOs1A6TuXpVZ0jESDGgY",
-    redirectUri:"https://melodic-cassata-2af0ea.netlify.app/home"
+    redirectUri: "https://melodic-cassata-2af0ea.netlify.app/home",
   });
+
   const handleSignup = (e) => {
     e.preventDefault();
-    setloader(true)
+    setloader(true);
     const { email, password, full_name } = formData;
     webAuth.signup(
       {
@@ -41,6 +189,7 @@ function SignupPage({setloader}) {
         if (err) {
           console.error("Error signing up:", err);
           alert("Error signing up. Please try again later.");
+          setloader(false);
           return;
         }
 
@@ -52,7 +201,6 @@ function SignupPage({setloader}) {
   };
 
   const handleLogin = (email, password) => {
-
     webAuth.login(
       {
         realm: "JSON-Validator",
@@ -64,22 +212,19 @@ function SignupPage({setloader}) {
         if (err) {
           console.error("Error logging in:", err);
           alert("Error logging in. Please check your credentials.");
+          setloader(false);
           return;
         }
         // navigate("/home")
-        }
+      }
     );
   };
-
 
   return (
     <>
       <div className="flex justify-center items-center w-full h-[600px]">
         <div className="border- border-[2px] max-w-[500px] w-full  rounded-[20px]">
-          <form
-            onSubmit={handleSignup}
-            className="max-w-[500px] w-full p-[2px]"
-          >
+          <form onSubmit={handleSignup} className="max-w-[500px] w-full p-[2px]">
             <div className="bg-sky-500  rounded-[20px] text-white">
               <h3 className="max-w-[250px] w-full items-center text-center font-bold text-[30px] py-4 ">
                 Sign Up
@@ -124,7 +269,7 @@ function SignupPage({setloader}) {
               <div className="d-grid flex justify-center items-center">
                 <button
                   type="submit"
-                  className="btn btn-primary border-[2px] font-bold text-sky-500 hover:text-white text-[16px]  rounded-[10px] max-w-[200px] w-full text-center  py-[5px]  hover:bg-sky-500"
+                  className="btn btn-primary border-[2px] font-bold text-sky-500 hover:text-white text-[16px] rounded-[10px] max-w-[200px] w-full text-center  py-[5px]  hover:bg-sky-500"
                 >
                   Sign Up
                 </button>
