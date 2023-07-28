73d9f2ba12258ba8fb9c9029d073a091e6232ec9
ALi <Ha3er125@teachers.org>
2023-07-28 22:21:58 +0500

    fixes in App.js

--- src/App.js
+++ src/App.js
@@ -178,20 +178,71 @@ import All_Components from "./Components/All_Components";
 import SignupPage from "./Features/Signup";
 import LoginPopup from "./Features/LoginPopup";
 import Page_404 from "./Components/Page_404";
+import { useEffect ,useState} from "react";
+import auth0 from "auth0-js";
+
 
 const domain = "techtribe.us.auth0.com";
 const clientId = "ffbSF4A20lHnWOs1A6TuXpVZ0jESDGgY";
 
 function App() {
-  const [loader, setloader] = React.useState(false); // Define the setloader function here
+  const [loader, setloader] = React.useState(false); 
+    const [userProfile, setUserProfile] = useState(null);
+      const uData = JSON.parse(sessionStorage.getItem("username"));
+
+
+
+  useEffect(() => {
+        const webAuth = new auth0.WebAuth({
+          domain: "techtribe.us.auth0.com",
+          clientID: "ffbSF4A20lHnWOs1A6TuXpVZ0jESDGgY",
+          redirectUri: "https://https://melodic-cassata-2af0ea.netlify.app/home",
+        });
+        const parseAccessToken = () => {
+          const hash = window.location.hash;
+          const tokenIndex = hash.indexOf("access_token=");
+          if (tokenIndex !== -1) {
+            const endTokenIndex = hash.indexOf("&", tokenIndex);
+            const accessToken = hash.substring(
+              tokenIndex + "access_token=".length,
+              endTokenIndex !== -1 ? endTokenIndex : undefined
+            );
+            return accessToken;
+          }
+          return null;
+        };
+    
+        const accessToken = parseAccessToken();
+        if (accessToken) {
+          webAuth.client.userInfo(accessToken, function (err, user) {
+            if (err) {
+              console.error("Error fetching user profile:", err);
+              return;
+            }
+    
+            // Store the user profile in state
+            setUserProfile(user);
+            sessionStorage.setItem("username", JSON.stringify(user));
+            console.log(user, "ali");
+          });
+    
+          if (uData) {
+            // navigate("/home");
+            console.log("xxxxxxgxx");
+          }
+        }
+      }, [loader]);
+
+
+
 
   return (
     <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
       
         <Routes>
           <Route path="/" element={<LoginPopup />} />
-          <Route path="/signup" element={<SignupPage setloader={setloader} />} /> {/* Pass setloader as a prop */}
-          <Route path="/home" element={<All_Components />} />
+          <Route path="/signup" element={<SignupPage setloader={setloader} />} /> 
+          <Route path="/home" element={<All_Components />}/>
           <Route path="/*" element={<Page_404 />} />
         </Routes>
      
